0dc7f4f36714e67d88fd22dae227dcb217081b8f
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-03-19 15:35:57 +0100

    Upgrade dev dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -4,36 +4,21 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-const baseRollupConfig = {
+
+export default {
     input: 'src/index.js',
-    plugins: []
+    external: [
+        '@lwc/engine',
+        '@lwc/wire-service'
+    ],
+    output: [
+        {
+            format: 'es',
+            file: 'dist/wire-service-jest-util.es.js',
+        },
+        {
+            format: 'cjs',
+            file: 'dist/wire-service-jest-util.common.js'
+        }
+    ]
 };
-
-const moduleName = 'wire-service-jest-util';
-
-function rollupConfig(format) {
-    let formatSuffix = '';
-    if (format === 'cjs') {
-        formatSuffix = '.common';
-    } else if (format === 'es') {
-        formatSuffix = '.es';
-    }
-
-    const targetName = [
-        'dist/',
-        moduleName,
-        formatSuffix,
-        '.js'
-    ].join('');
-
-    return Object.assign({}, baseRollupConfig, {
-        output: {
-            'file': targetName,
-            format
-        }});
-}
-
-export default [
-    rollupConfig('es'),
-    rollupConfig('cjs')
-];
