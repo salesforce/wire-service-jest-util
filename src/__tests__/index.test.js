41914dc26887454807a1d6af9a56726fd8deced1
Jose David Rodriguez Velasco <jodarove@gmail.com>
2020-04-13 16:32:42 -0700

    wip: more tests and copyright header

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -41,6 +41,57 @@ describe('registerLdsTestWireAdapter', () => {
         expect(testWireAdapter).toHaveProperty('getLastConfig');
     });
 
+    it('should emit values', () => {
+        const testWireAdapter = target.registerLdsTestWireAdapter(adapterMock);
+
+        const dataCallbackMock = jest.fn();
+        // mock the lwc engine wiring the adapter to the component.
+        // eslint-disable-next-line no-unused-vars
+        const wireInstance = new adapterMock.adapter(dataCallbackMock);
+        const testValue = { test: 'value' };
+        testWireAdapter.emit(testValue);
+
+        expect(dataCallbackMock).toHaveBeenCalled();
+        expect(dataCallbackMock).toHaveBeenCalledWith({ data: testValue, error: undefined });
+    });
+
+    it('should emit error', () => {
+        const testWireAdapter = target.registerLdsTestWireAdapter(adapterMock);
+
+        const dataCallbackMock = jest.fn();
+        // mock the lwc engine wiring the adapter to the component.
+        // eslint-disable-next-line no-unused-vars
+        const wireInstance = new adapterMock.adapter(dataCallbackMock);
+        testWireAdapter.error('body', 'status', 'text');
+
+        expect(dataCallbackMock).toHaveBeenCalled();
+        const expectedError = {
+            "body": "body",
+            "ok": false,
+            "status": "status",
+            "statusText": "text",
+        };
+
+        expect(dataCallbackMock).toHaveBeenCalledWith({ data: undefined, error: expectedError });
+    });
+
+    it('should get last config', () => {
+        const testWireAdapter = target.registerLdsTestWireAdapter(adapterMock);
+
+        // mock the lwc engine wiring the adapter to the component.
+        const wireInstance = new adapterMock.adapter(jest.fn());
+        const firstConfig = { v: 'first' };
+        const secondConfig = { v: 'second' };
+
+        wireInstance.update(firstConfig);
+
+        expect(testWireAdapter.getLastConfig()).toBe(firstConfig);
+
+        wireInstance.update(secondConfig);
+
+        expect(testWireAdapter.getLastConfig()).toBe(secondConfig);
+    });
+
     it('throws error when no adapter id', () => {
         expect(() => {
             target.registerLdsTestWireAdapter();
@@ -65,6 +116,37 @@ describe('registerTestWireAdapter', () => {
         expect(testWireAdapter).toHaveProperty('getLastConfig');
     });
 
+    it('should emit values', () => {
+        const testWireAdapter = target.registerTestWireAdapter(adapterMock);
+
+        const dataCallbackMock = jest.fn();
+        // mock the lwc engine wiring the adapter to the component.
+        // eslint-disable-next-line no-unused-vars
+        const wireInstance = new adapterMock.adapter(dataCallbackMock);
+        const testValue = { test: 'value' };
+        testWireAdapter.emit(testValue);
+
+        expect(dataCallbackMock).toHaveBeenCalled();
+        expect(dataCallbackMock).toHaveBeenCalledWith(testValue);
+    });
+
+    it('should get last config', () => {
+        const testWireAdapter = target.registerTestWireAdapter(adapterMock);
+
+        // mock the lwc engine wiring the adapter to the component.
+        const wireInstance = new adapterMock.adapter(jest.fn());
+        const firstConfig = { v: 'first' };
+        const secondConfig = { v: 'second' };
+
+        wireInstance.update(firstConfig);
+
+        expect(testWireAdapter.getLastConfig()).toBe(firstConfig);
+
+        wireInstance.update(secondConfig);
+
+        expect(testWireAdapter.getLastConfig()).toBe(secondConfig);
+    });
+
     it('throws error when no adapter id', () => {
         expect(() => {
             target.registerTestWireAdapter();
@@ -74,6 +156,93 @@ describe('registerTestWireAdapter', () => {
     it('throws error when no valid mock', () => {
         expect(() => {
             target.registerTestWireAdapter(function () {});
+        }).toThrow('adapterId should be a jest mock function. Please update your mocks and use ' +
+            'createWireAdapterMock to mock wire adapters.');
+    });
+});
+
+describe('registerApexTestWireAdapter', () => {
+    const apexCallMock = jest.fn();
+    const adapterMock = target.createWireAdapterMock(apexCallMock);
+
+    it('returns a test wire adapter', () => {
+        const testWireAdapter = target.registerApexTestWireAdapter(adapterMock);
+        expect(testWireAdapter).toHaveProperty('emit');
+        expect(testWireAdapter).toHaveProperty('error');
+        expect(testWireAdapter).toHaveProperty('getLastConfig');
+    });
+
+    it('apex can be used as callable', () => {
+        apexCallMock.mockClear();
+
+        const apexParameter1 = { a: 'b' };
+        const apexParameter2 = { c: 'd' };
+        adapterMock(apexParameter1, apexParameter2);
+
+        expect(apexCallMock).toHaveBeenCalled();
+        expect(apexCallMock).toHaveBeenCalledWith(apexParameter1, apexParameter2);
+    });
+
+    it('should emit values', () => {
+        const testWireAdapter = target.registerApexTestWireAdapter(adapterMock);
+
+        const dataCallbackMock = jest.fn();
+        // mock the lwc engine wiring the adapter to the component.
+        // eslint-disable-next-line no-unused-vars
+        const wireInstance = new adapterMock.adapter(dataCallbackMock);
+        const testValue = { test: 'value' };
+        testWireAdapter.emit(testValue);
+
+        expect(dataCallbackMock).toHaveBeenCalled();
+        expect(dataCallbackMock).toHaveBeenCalledWith({ data: testValue, error: undefined });
+    });
+
+    it('should emit error', () => {
+        const testWireAdapter = target.registerApexTestWireAdapter(adapterMock);
+
+        const dataCallbackMock = jest.fn();
+        // mock the lwc engine wiring the adapter to the component.
+        // eslint-disable-next-line no-unused-vars
+        const wireInstance = new adapterMock.adapter(dataCallbackMock);
+        testWireAdapter.error('body', 'status', 'text');
+
+        expect(dataCallbackMock).toHaveBeenCalled();
+        const expectedError = {
+            "body": "body",
+            "ok": false,
+            "status": "status",
+            "statusText": "text",
+        };
+
+        expect(dataCallbackMock).toHaveBeenCalledWith({ data: undefined, error: expectedError });
+    });
+
+    it('should get last config', () => {
+        const testWireAdapter = target.registerApexTestWireAdapter(adapterMock);
+
+        // mock the lwc engine wiring the adapter to the component.
+        const wireInstance = new adapterMock.adapter(jest.fn());
+        const firstConfig = { v: 'first' };
+        const secondConfig = { v: 'second' };
+
+        wireInstance.update(firstConfig);
+
+        expect(testWireAdapter.getLastConfig()).toBe(firstConfig);
+
+        wireInstance.update(secondConfig);
+
+        expect(testWireAdapter.getLastConfig()).toBe(secondConfig);
+    });
+
+    it('throws error when no adapter id', () => {
+        expect(() => {
+            target.registerApexTestWireAdapter();
+        }).toThrow('No adapter specified');
+    });
+
+    it('throws error when no valid mock', () => {
+        expect(() => {
+            target.registerApexTestWireAdapter(function () {});
         }).toThrow('adapterId should be a jest mock function. Please update your mocks and use ' +
             'createWireAdapterMock to mock wire adapters.');
     });
