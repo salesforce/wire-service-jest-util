5ab726ec76fb00d3bef36afe7ea032354e0cf535
Jose David Rodriguez Velasco <jodarove@gmail.com>
2021-03-03 12:07:17 -0800

    fix: transform class properties

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,16 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+import { babel } from '@rollup/plugin-babel';
 
 export default {
     input: 'src/index.js',
+    plugins: [
+        babel({
+            babelHelpers: 'bundled',
+            plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]]
+        }),
+    ],
     external: [
         '@lwc/engine',
         '@lwc/wire-service'
